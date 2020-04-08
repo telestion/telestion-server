249e415de6d3275b23150d78487b9d9f18efc898
dankondr <dankondr@icloud.com>
2020-04-08 17:58:20 +0300

    ✨Add delete and delete_and_commit methods to ModelWithMethods in models.py

--- src/server/db/models.py
+++ src/server/db/models.py
@@ -16,14 +16,24 @@ class ModelWithMethods(db.Model):
         db.session.add(self)
 
     def add_and_commit(self):
-        db.session.add(self)
+        self.add()
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+
+    def delete_and_commit(self):
+        self.delete()
         db.session.commit()
 
 
 class Question(ModelWithMethods):
     __tablename__ = 'question'
     id = db.Column(db.Integer, primary_key=True)
+    # TODO: - Make created_by = Integer ForeignKey('user.id)
     created_by = db.Column(db.String, index=True, nullable=False)
+    # TODO: - Add addresser = Integer ForeignKey('user.id')
+    # addresser = db.Column(db.)
     text = db.Column(db.String, nullable=False)
     date = db.Column(db.DateTime, nullable=False)
 
@@ -40,6 +50,7 @@ class Answer(ModelWithMethods):
     __tablename__ = 'answer'
     id = db.Column(db.Integer, primary_key=True)
     question_id = db.Column(db.Integer, db.ForeignKey('question.id'))
+    # TODO: - Make created_by = Integer ForeignKey('user.id)
     created_by = db.Column(db.String, index=True, nullable=False)
     text = db.Column(db.String, nullable=False)
     date = db.Column(db.DateTime, nullable=False)
